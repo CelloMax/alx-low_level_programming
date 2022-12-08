8f7e1770a0d7e71fbc3fe23a80aa120ed63be4a9
CelloMax <achichuks3@gmail.com>
2022-12-08 14:35:20 -0800

    test3

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,21 @@
+#include <studio.h>
+
+/**
+ *
+ *  * main - Entry point
+ *
+ *   *
+ *
+ *    * Return: Always 0 (success)
+ *
+ *     */
+
+int main(void)
+
+{
+		printf("with proper grammar, but the outcome is a piece of art,\n");
+
+			return (0);
+
+
+}
