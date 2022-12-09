4797a80baffeba6de979b81777ca09157af0d64d
CelloMax <115787739+CelloMax@users.noreply.github.com>
2022-12-09 22:37:27 +0100

    Update 3-print_alphabets.c
    test3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Description:program that  prints the alphabet in lowercase,
+ * and then in uppercase
+ * Return: 0
+ */
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+
+	c = 'A';
+
+	while (c <= 'Z')
+	{
+		putchar(c);
+		c++;
+	}
+
+	putchar('\n');
+	return (0);
+}
