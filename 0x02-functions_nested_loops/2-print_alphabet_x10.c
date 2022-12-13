4c389c13efecc97ce28f4d584e5028cb89f2ed19
CelloMax <achichuks3@gmail.com>
2022-12-13 06:21:12 -0800

    test3

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print a - z ten times
+ */
+
+void print_alphabet_x10(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 'a'; j = < 'z'; j++)
+		{
+			_putchar(j);
+		}
+
+		_putchar('\n');
+	}
+
+	return (0);
+}
