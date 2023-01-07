202ca86f1cceacb3d950f6c380fa7bf5c87111ed
CelloMax <115787739+CelloMax@users.noreply.github.com>
2023-01-07 15:22:25 +0100

    0

--- 0x09-static_libraries/0-memset.c
+++ 0x09-static_libraries/0-memset.c
@@ -1,22 +1,20 @@
 #include "main.h"
 /**
- *_memset - fills memory with a constant byte.
- *fills the first n bytes of the memory area pointed
- *to by s with the constant b.
- *@s: pointer to memory area.
- *@b: constant to fill memory with.
- *@n: bytes of the memory area to be filled.
- *
- *Return: pointer to memory area s.
+*_memset - The _memset() function fills
+* the first n bytes of the memory area
+* pointed to by s with the constant byte b
+*@s:target
+*@b: constant byte
+*@n:number of byte
+*Return: returns new value of target
 */
+
 char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int counter;
-
-	for (counter = 0; counter < n; counter++)
+	while (n)
 	{
-		s[counter] = b;
+		s[n - 1] = b;
+		n--;
 	}
 	return (s);
 }
-
