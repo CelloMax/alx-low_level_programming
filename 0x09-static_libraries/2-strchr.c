94670c51ee6e727e6f24c85dc266f42ef5206101
CelloMax <115787739+CelloMax@users.noreply.github.com>
2023-01-07 15:32:12 +0100

    2

--- 0x09-static_libraries/2-strchr.c
+++ 0x09-static_libraries/2-strchr.c
@@ -1,24 +1,23 @@
 #include "main.h"
-#define NULL 0
-
+#include <stddef.h>
 /**
-* _strchr - locate 1st occurrence of char in string and returns pointer there
-* @s: string to search
-* @c: target characer
-* Return: pointer to that character in string
+*_strchr - Returns a pointer to the first occurrence
+*of the character c in the string s, or NULL if the
+*character is not found
+*
+*@s:string targeted
+*@c:character targeted
+*
+*Return: returns pointer to first occcurence of c
 */
-
 char *_strchr(char *s, char c)
 {
-	int i = 0;
+	int i;
 
-	while (s[i] != '\0' && s[i] != c)
-		i++;
-
+	for (i = 0; (s[i] != c) && (s[i] != '\0'); i++)
+		;
 	if (s[i] == c)
-		return (&s[i]);
-
+		return (s + i);
 	else
 		return (NULL);
 }
-
