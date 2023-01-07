0b27f665de529c280e088e6c99fce8b59e137568
CelloMax <achichuks3@gmail.com>
2023-01-07 15:04:07 +0100

    s

--- 0x09-static_libraries/0-isupper.c
+++ 0x09-static_libraries/0-isupper.c
@@ -1,20 +1,14 @@
 #include "main.h"
-
 /**
- * _isupper - checks uppercase
- * @c: parameter
- * Return: 0 if successful
- */
+*_isupper - function that verifies if a  character is uppercase or not
+*@c: tested character
+*Return: returns 1 if it is uppercase , 0 if not
+*/
 
 int _isupper(int c)
 {
-	if (c >= 65 && c < 91)
-	{
+	if ((c >= 'A') && (c <= 'Z'))
 		return (1);
-	}
-	else
-	{
-		return (0);
-	}
-}
 
+	return (0);
+}
