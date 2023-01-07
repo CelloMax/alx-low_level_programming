150e696dd34300b93ab44a2d6590503b0ba79650
CelloMax <115787739+CelloMax@users.noreply.github.com>
2023-01-07 15:44:22 +0100

    4

--- 0x09-static_libraries/4-strpbrk.c
+++ 0x09-static_libraries/4-strpbrk.c
@@ -1,33 +1,29 @@
 #include "main.h"
-#define NULL 0
-
 /**
-* _strpbrk - return pointer to byte in s that matches a byte in accept
-* @s: string to search
-* @accept: target matches
-* Return: pointer to index of string at first occurence
+*_strpbrk - The _strpbrk() function locates the first
+* occurrence in the string s of any of the bytes in
+*the string accept
+*
+*@s: string where search is made
+*@accept: string where searched bytes are located
+*
+*Return:Returns a pointer to the byte in s that matches
+* one of the bytes in accept, or NULL if no such byte is found
 */
 
 char *_strpbrk(char *s, char *accept)
 {
-	int x = 0, y;
+	int i;
 
-	while (s[x] != '\0')
+	while (*s)
 	{
-		for (y = 0; accept[y] != '\0'; y++)
+		for (i = 0; accept[i]; i++)
 		{
-			if (s[x] == accept[y])
-			{
-				s = &s[x];
+			if (accept[i] == *s)
 				return (s);
-			}
-
 		}
-
-	x++;
+		s++;
 	}
 
-	return (NULL);
-
+	return ('\0');
 }
-
