84329cb89c11dee9eb9fd8916fa1905821793f01
CelloMax <achichuks3@gmail.com>
2023-01-07 14:25:30 +0100

    all files to be pushed

---
+++ 0x09-static_libraries/1-isdigit.c
@@ -0,0 +1,18 @@
+/**
+ * _isdigit - checks for digits
+ * @c: parameter
+ * Return: 0 or 1
+ */
+
+int _isdigit(int c)
+{
+	if (c >= 48 && c < 58)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
+
